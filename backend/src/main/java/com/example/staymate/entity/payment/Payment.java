d96c5cc6a09036db04761057f4d258da4b14613e
NgZhiKai <78411240+NgZhiKai@users.noreply.github.com>
2025-03-22 14:31:39 +0800

    changed payment to manytoone to booking

--- backend/src/main/java/com/example/staymate/entity/payment/Payment.java
+++ backend/src/main/java/com/example/staymate/entity/payment/Payment.java
@@ -13,7 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Payment {
@@ -21,7 +21,7 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "booking_id")
     private Booking booking;
 
