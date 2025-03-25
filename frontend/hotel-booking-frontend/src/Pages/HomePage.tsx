3e20b45512c8696168eff90b7230e757e48ab5e7
JK-NCS <jukhang.ng@ncs.com.sg>
2025-03-25 16:53:30 +0800

    Update HomePage.tsx

--- frontend/hotel-booking-frontend/src/Pages/HomePage.tsx
+++ frontend/hotel-booking-frontend/src/Pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="pt-8"> {/* Added top padding */}
+    <div className="pt-0" style={{ paddingLeft: '20px' }}> {/* Added top and left padding */}
       <h1 className="text-3xl font-bold">Welcome to StayMate</h1>
       <p className="text-lg mt-2">Find the best hotels near you!</p>
     </div>
