4d1ebc13af42e677f7a6b4bd8d8926943f89c7d2
waterxiv <jukhang.ng@ncs.com.sg>
2025-04-04 21:55:49 +0800

    Update HotelCard.tsx

--- frontend/hotel-booking-frontend/src/components/HotelCard.tsx
+++ frontend/hotel-booking-frontend/src/components/HotelCard.tsx
@@ -1,7 +1,7 @@
-// src/components/HotelCard.tsx
 import React from 'react';
 import { Card, CardContent, CardMedia, Typography, Box, Rating } from '@mui/material';
 import { Hotel } from '../types/Hotels';
+import { Link } from 'react-router-dom';
 
 interface HotelCardProps {
   hotel: Hotel;
@@ -9,29 +9,26 @@ interface HotelCardProps {
 
 const HotelCard: React.FC<HotelCardProps> = ({ hotel }) => {
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="140"
-        image={hotel.image}
-        alt={hotel.name}
-      />
-      <CardContent>
-        <Typography variant="h5">{hotel.name}</Typography>
-        <Typography variant="body2" color="text.secondary">
-          {hotel.address}
-        </Typography>
-        <Box display="flex" alignItems="center" mt={1}>
-          <Rating value={hotel.rating} readOnly precision={0.1} />
-          <Typography variant="body2" ml={1}>
-            {hotel.rating}
+    <Link to={`/hotel/${hotel.id}`} style={{ textDecoration: 'none' }}>
+      <Card sx={{ maxWidth: 345, cursor: 'pointer' }}>
+        <CardMedia component="img" height="140" image={hotel.image} alt={hotel.name} />
+        <CardContent>
+          <Typography variant="h5">{hotel.name}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {hotel.address}
           </Typography>
-        </Box>
-        <Typography variant="h6" mt={1}>
-          ${hotel.pricePerNight} / night
-        </Typography>
-      </CardContent>
-    </Card>
+          <Box display="flex" alignItems="center" mt={1}>
+            <Rating value={hotel.rating} readOnly precision={0.1} />
+            <Typography variant="body2" ml={1}>
+              {hotel.rating}
+            </Typography>
+          </Box>
+          <Typography variant="h6" mt={1}>
+            ${hotel.pricePerNight} / night
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
