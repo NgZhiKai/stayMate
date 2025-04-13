90892e22504964362110192046a2ec7bdf21897a
NgZhiKai <78411240+NgZhiKai@users.noreply.github.com>
2025-04-13 09:57:49 +0800

    test frontend deploy

--- backend/src/main/java/com/example/staymate/config/WebConfig.java
+++ backend/src/main/java/com/example/staymate/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ngzhikai.github.io")
+                .allowedOrigins(baseUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
